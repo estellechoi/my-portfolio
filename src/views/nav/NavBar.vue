38d731fe82bd5513a78d1f152cf6f45be826b5f5
Estelle Choi <estele.choi@gmail.com>
2021-08-16 21:01:49 +0900

    [View] NavBar.vue got a new class 'active' which is toggled by user

--- src/views/nav/NavBar.vue
+++ src/views/nav/NavBar.vue
@@ -1,17 +1,40 @@
 <template>
-  <nav class="navbar flex justify-between items-center">
-    <div class="navbar__logo">sunkiss</div>
-    <ul class="hidden">
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
+  <nav
+    :class="[
+      'navbar flex justify-between items-center',
+      showNavMenu ? 'active' : '',
+    ]"
+  >
+    <div class="navbar__logo">estelle</div>
+    <BurgerMenuButton @toggle="toggleNavMenu" />
+    <ul :class="['flex flex-col justify-center items-center', 'navbar-menu']">
+      <li class="navbar-menu__item"><router-link to="/">Home</router-link></li>
+      <li class="navbar-menu__item"
+        ><router-link to="/about">About</router-link></li
+      >
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import BurgerMenuButton from '@/components/buttons/BurgerMenuButton.vue'
 
-export default defineComponent({})
+export default defineComponent({
+  components: { BurgerMenuButton },
+  data() {
+    return {
+      showNavMenu: false,
+    }
+  },
+  methods: {
+    toggleNavMenu(val: { active: boolean }) {
+      console.log('toggleNavMenu', val)
+      this.showNavMenu = val.active
+      // ..
+    },
+  },
+})
 </script>
 
 <style scoped lang="scss">
@@ -22,18 +45,46 @@ export default defineComponent({})
   right: 0;
   bottom: auto;
   width: 100vw;
-  height: 60px;
+  height: var(--size-navbar-h);
   z-index: 9999;
   color: var(--color-white);
   background-color: var(--color-primary-opacity);
   backdrop-filter: blur(6px) opacity(1);
   border-bottom: 1px solid var(--color-white);
   padding: var(--padding-nav-v) var(--padding-nav-h);
+  // transition: all 0.15s;
 
   .navbar__logo {
     font-size: 34px;
     font-weight: var(--font-weight-bold);
     letter-spacing: var(--letter-spacing-narrow);
+  }
+
+  .navbar-menu {
+    position: fixed;
+    top: var(--size-navbar-h);
+    left: 0;
+    width: 100vw;
+    height: calc(100vh - var(--size-navbar-h));
+    background-color: var(--color-white);
+    opacity: 0;
+    z-index: -1;
+    transition: all var(--animation-duration-s);
+
+    .navbar-menu__item {
+      color: var(--color-black);
+    }
+  }
+
+  &.active {
+    color: var(--color-black);
+    background-color: var(--color-white);
+    backdrop-filter: none;
+
+    .navbar-menu {
+      opacity: 1;
+      z-index: 0;
+    }
   }
 }
 </style>
