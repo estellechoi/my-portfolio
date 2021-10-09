91ef3230860a06e76c83ec6e84a6a1dd807e9c5c
Estelle Choi <estele.choi@gmail.com>
2021-10-10 05:12:17 +0900

    [New] Works route view component created

---
+++ src/views/Works.vue
@@ -0,0 +1,40 @@
+<template>
+  <MainPage class="page">
+    <button @click="click">Popup</button>
+    <DonationPopup v-if="showDonationPopup" />
+  </MainPage>
+</template>
+
+<script lang="ts" scoped>
+import { defineComponent } from 'vue'
+import MainPage from '@/components/pages/MainPage.vue'
+import DonationPopup from '@/views/common/DonationPopup.vue'
+
+export default defineComponent({
+  components: { MainPage, DonationPopup },
+  data() {
+    return {
+      showDonationPopup: false,
+    }
+  },
+  methods: {
+    click() {
+      this.showDonationPopup = true
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/modules.scss';
+
+.page {
+  min-height: 100vh;
+  background-color: var(--color-white);
+  background-image: url(./../assets/bg/bg-me-in-paris.jpeg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-x: 40%;
+  background-attachment: scroll;
+}
+</style>
