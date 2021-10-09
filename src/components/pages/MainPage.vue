532139f813a4741996bd17020d0ed9be25efffc6
Estelle Choi <estele.choi@gmail.com>
2021-10-10 05:08:25 +0900

    [New] main page component

---
+++ src/components/pages/MainPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="page" role="main"><slot /></main>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  padding: var(--size-header-h) var(--padding-page-h-s);
+}
+</style>
