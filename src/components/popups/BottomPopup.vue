b47b6bf1cc037a2b566be95c8aa6f0da411f6e8b
Estelle Choi <estele.choi@gmail.com>
2021-10-10 05:09:03 +0900

    [WIP] a popup sliding from bottom creaed but work in progress

---
+++ src/components/popups/BottomPopup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="popup" role="dialog">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" scoped>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/modules.scss';
+
+.popup {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  background: var(--color-white);
+  padding: var(--padding-page-v-top) var(--padding-page-h-s);
+  border-top-left-radius: var(--radius-m);
+  border-top-right-radius: var(--radius-m);
+  box-shadow: $box-shadow-base;
+  animation: popup-from-bottom $animation-duration-s both;
+}
+
+@keyframes popup-from-bottom {
+  0% {
+    transform: translateY(600);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
